0fefb7dc1e169909c67b5d86efbf618c4ca4d4a5
lukebayes <lbayes@patternpark.com>
2010-07-19 19:17:55 -0700

    Actually fixed failing test with relative paths

--- test/unit/mxmlc_test.rb
+++ test/unit/mxmlc_test.rb
@@ -19,7 +19,7 @@ class MXMLCTest < Test::Unit::TestCase
     should "accept input" do
       as_a_unix_system do
         mxmlc = FlashSDK::MXMLC.new
-        mxmlc.input = @input
+        mxmlc.input = 'test/fixtures/mxmlc/simple/SomeFile.as'
         mxmlc.source_path << 'test/fixtures/mxmlc/simple'
         assert_equal '-source-path+=test/fixtures/mxmlc/simple -static-link-runtime-shared-libraries test/fixtures/mxmlc/simple/SomeFile.as', mxmlc.to_shell
       end
