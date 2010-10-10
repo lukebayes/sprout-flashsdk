15adb878000419b55dab53609f9426069449b5d1
lukebayes <lbayes@patternpark.com>
2010-10-09 23:17:19 -0700

    Updated FlashPlayer spec to use correct MD5 hashes

--- lib/flashplayer/specification.rb
+++ lib/flashplayer/specification.rb
@@ -8,7 +8,7 @@ Sprout::Specification.new do |s|
     t.platform     = :osx
     t.archive_type = :zip
     t.url          = "http://download.macromedia.com/pub/flashplayer/updaters/10/flashplayer_10_sa_debug.app.zip"
-    t.md5          = "ff6824b7fd676dd1b613204221f5b5b9"
+    t.md5          = "1c5ef8aeb1aa4a6cee7970d5c7ee9a55"
     t.add_executable :flashplayer, "Flash Player Debugger.app"
   end
 
@@ -16,7 +16,7 @@ Sprout::Specification.new do |s|
     t.platform     = :win32
     t.archive_type = :exe
     t.url          = "http://download.macromedia.com/pub/flashplayer/updaters/10/flashplayer_10_sa_debug.exe"
-    t.md5          = "4d8d58d72709f44421b2ea4e89cc30be"
+    t.md5            "1f5e411f96817b56c99111a06f4c727f"
     t.add_executable :flashplayer, "flashplayer_10_sa_debug.exe"
   end
 
@@ -24,7 +24,7 @@ Sprout::Specification.new do |s|
     t.platform     = :linux
     t.archive_type = :tgz
     t.url          = "http://download.macromedia.com/pub/flashplayer/updaters/10/flashplayer_10_sa_debug.tar.gz"
-    t.md5          = "6cabe6038343374b547043d29de14417"
+    t.md5          = "1efa254bc7d6e0c11a61984ae34bfaa7"
     t.add_executable :flashplayer, "flashplayerdebugger"
   end
 
