aa6f719cdc4310c2242be0631e8b49b0370e3a6f
lukebayes <lbayes@patternpark.com>
2010-10-12 09:01:55 -0700

    The order of the platform-specific file targets in the Flash Player specification is important

--- lib/flashplayer/specification.rb
+++ lib/flashplayer/specification.rb
@@ -4,6 +4,24 @@ Sprout::Specification.new do |s|
   s.name        = FlashPlayer::NAME
   s.version     = FlashSDK::VERSION
 
+  ##
+  # NOTE: The order of these declarations is important, the RubyFeature.load method
+  # will search for the first match that is appropriate for the end user system.
+  #
+  # Current releases of the Ruby One-Click Installer for Windows actually
+  # run on top of Mingw, and OSX is a *nix variant, which means that 
+  # all System types (in Ruby at least) derive from UnixSystem. 
+  #
+  # This means that the Linux/Unix declaration will
+  # match everyone, so it is effectively the same as ':universal'
+  s.add_remote_file_target do |t|
+    t.platform     = :windows
+    t.archive_type = :exe
+    t.url          = "http://download.macromedia.com/pub/flashplayer/updaters/10/flashplayer_10_sa_debug.exe"
+    t.md5          = "1f5e411f96817b56c99111a06f4c727f"
+    t.add_executable :flashplayer, "flashplayer_10_sa_debug.exe"
+  end
+
   s.add_remote_file_target do |t|
     t.platform     = :osx
     t.archive_type = :zip
@@ -13,20 +31,11 @@ Sprout::Specification.new do |s|
   end
 
   s.add_remote_file_target do |t|
-    t.platform     = :win32
-    t.archive_type = :exe
-    t.url          = "http://download.macromedia.com/pub/flashplayer/updaters/10/flashplayer_10_sa_debug.exe"
-    t.md5          = "1f5e411f96817b56c99111a06f4c727f"
-    t.add_executable :flashplayer, "flashplayer_10_sa_debug.exe"
-  end
-
-  s.add_remote_file_target do |t|
     t.platform     = :linux
     t.archive_type = :tgz
     t.url          = "http://download.macromedia.com/pub/flashplayer/updaters/10/flashplayer_10_sa_debug.tar.gz"
     t.md5          = "1efa254bc7d6e0c11a61984ae34bfaa7"
     t.add_executable :flashplayer, "flashplayerdebugger"
   end
-
 end
 
